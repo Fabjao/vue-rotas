f31414474cf69b3c3d78ba1a368cc71c61d6ace7
Fábio Gonçalves <fabiobasquete4@hotmail.com>
2019-02-19 00:08:41 -0300

    Continuação do curso

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes:[]
+})
 
 new Vue({
   render: h => h(App),
